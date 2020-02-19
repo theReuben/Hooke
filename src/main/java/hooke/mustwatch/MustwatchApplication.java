8dc08086e4c401969c999fc0df829bfaa5eca7d7
Reuben G Day <34290363+theReuben@users.noreply.github.com>
2020-02-19 20:12:09 +0000

    Add files via upload

---
+++ src/main/java/hooke/mustwatch/MustwatchApplication.java
@@ -0,0 +1,13 @@
+package hooke.mustwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MustwatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hooke/mustwatch/MustwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hooke.mustwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MustwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
